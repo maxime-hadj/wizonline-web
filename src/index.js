c5334f78f8d981e14cc0bd51b0e9f2b8cc02571f
Max <maxime.hadj@laplateforme.io>
2023-10-24 17:03:07 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as ReactDOMClient from "react-dom/client";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root");
+
+/** Uncomment if you use material-ui **/
+//import CssBaseline from "@mui/material/CssBaseline";
+
+/** Uncomment if you use react-router-dom **/
+//import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOMClient.createRoot(container);
+
 root.render(
   <React.StrictMode>
+    {/** Uncomment if you use react-redux **/}
+    {/* <Provider store={store}> */}
+    {/** Uncomment if you use material-ui **/}
+    {/*<CssBaseline />*/}
+    {/** Uncomment if you use react-router **/}
+    {/*<BrowserRouter>*/}
     <App />
+    {/** Uncomment if you use react-router **/}
+    {/*</BrowserRouter>*/}
+    {/** Uncomment if you use react-redux **/}
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
